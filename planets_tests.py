9d7ecb9081b51809e06fbe5d4f2b157ff3e41249
Lemar Popal <lepopal@unix4.csc.calpoly.edu>
2018-09-23 22:06:33 -0700

    first commit

--- planets_tests.py
+++ planets_tests.py
@@ -6,12 +6,28 @@ class Test_planets(unittest.TestCase):
 
     def test_01(self):
         sys.stdin = io.StringIO("136")
-        sys.stdout = student_output = io.StringIO()        
+        sys.stdout = student_output = io.StringIO()
         expected_out = "What do you weigh on earth? \nOn Mars you would weigh 51.68 pounds.\nOn Jupiter you would weigh 318.24 pounds."
         weight_on_planets()
         #print(student_output.getvalue().strip())
         self.assertEqual(expected_out, student_output.getvalue().strip())
 
+    def test_02(self):
+        sys.stdin = io.StringIO("0.0000")
+        sys.stdout = student_output = io.StringIO()
+        expected_out = "What do you weigh on earth? \nOn Mars you would weigh 0.00 pounds.\nOn Jupiter you would weigh 0.00 pounds."
+        weight_on_planets()
+        #print(student_output.getvalue().strip())
+        self.assertEqual(expected_out, student_output.getvalue().strip())
+
+    #testing negative numbers just in case
+    def test_03(self):
+        sys.stdin = io.StringIO("-4.8123")
+        sys.stdout = student_output = io.StringIO()
+        expected_out = "What do you weigh on earth? \nOn Mars you would weigh -1.83 pounds.\nOn Jupiter you would weigh -11.26 pounds."
+        weight_on_planets()
+        #print(student_output.getvalue().strip())
+        self.assertEqual(expected_out, student_output.getvalue().strip())
 
 if __name__ == "__main__":
         unittest.main()
